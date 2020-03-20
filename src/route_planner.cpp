b08eb1ffb2f53f7fd25e00c5f2193c859fa10157
zh2eou <zh2eou@gmail.com>
2020-03-19 18:28:59 -0700

    Update route_planner.cpp
    Fix calculation of g value in AddNeighbors

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -12,6 +12,7 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
     start_node = &model.FindClosestNode(start_x, start_y);
     end_node = &model.FindClosestNode(end_x, end_y);
+// 	std::cout<< "start node x, y " << start_node->x << ", " <<start_node->y<< " \n";
 
 }
 
@@ -37,7 +38,7 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
     current_node->FindNeighbors();
     for (RouteModel::Node *node : current_node->neighbors) {
         node->parent = current_node;
-        node->g_value += current_node->distance(*node);
+        node->g_value = current_node->g_value + current_node->distance(*node);
         node->h_value = CalculateHValue(node);
         open_list.push_back(node);
         node->visited = true; 
@@ -65,14 +66,15 @@ RouteModel::Node *RoutePlanner::NextNode() {
     RouteModel::Node *nextNode = open_list[open_list.size() - 1];
 //     std::cout << "START: ";
 //     for (RouteModel::Node *node : open_list) {
-//         std::cout << "=" << node->h_value + node->g_value << "=";
+//         std::cout<< "== x, y, sum: " << node->x << ", " <<node->y<< " , "<< node->h_value + node->g_value <<" ==";
 //     }
     open_list.pop_back();
-//     std::cout << "\nEND:";
 //     for (auto node : open_list) {
 //         std::cout << "=" << node->h_value + node->g_value << "=";
 //     }
-//     std::cout << "\n";
+//     std::cout << " :END\n";
+//     std::cout<< "Next node x, y, sum: " << nextNode->x << ", " <<nextNode->y<< " " << nextNode->h_value + nextNode->g_value << " \n";
+//     std::cout << "\n\n";
     return nextNode;
 }
 
@@ -91,19 +93,25 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     std::vector<RouteModel::Node> path_found;
 
     // TODO: Implement your solution here.
+//     std::cout << "Start node x, y: " << start_node->x << ", " << start_node->y << "\n";
     while (current_node != start_node) {
       distance += current_node->distance(*current_node->parent);
       path_found.emplace(path_found.begin(), *current_node);
       current_node = current_node->parent;
     }
     path_found.emplace(path_found.begin(), *current_node);
+    int cnt = 1;
+
 //     for (RouteModel::Node node : path_found) {
-//       std::cout << " x, y: " << node.x << ", " << node.y << "\n";
+//       std::cout << "node " << cnt << " x, y: " << node.x << ", " << node.y << "\n";
+//       cnt++;
 //     }
+//     std::cout << "End node x, y: " << end_node->x << ", " << end_node->y<< "\n";
+
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
 
-}
+} 
 
 
 // TODO 7: Write the A* Search algorithm here.
@@ -120,11 +128,12 @@ void RoutePlanner::AStarSearch() {
     // Set initial node to be start node, set visited to true
     current_node = start_node;
     current_node->visited = true;
-    AddNeighbors(current_node);
-  
+    //AddNeighbors(current_node);
+    open_list.push_back(start_node);
     // Keep looking for next node until you get to the end node
-    while (current_node != end_node) {
+    while (current_node != end_node && open_list.size() != 0) {
         current_node = NextNode();
+//         std :: cout << "current node: g = " << current_node->g_value << " h = " << current_node->h_value << "\n"; 
         AddNeighbors(current_node);
 
     }
